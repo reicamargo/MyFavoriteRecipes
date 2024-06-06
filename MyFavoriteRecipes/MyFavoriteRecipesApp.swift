7b0dfbc32814fd40ab1cea0a5ed1ebeeba8d2ee1
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-06-06 19:44:31 -0300

    Initial Commit

---
+++ MyFavoriteRecipes/MyFavoriteRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteRecipesApp.swift
+//  MyFavoriteRecipes
+//
+//  Created by Reinaldo Camargo on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
