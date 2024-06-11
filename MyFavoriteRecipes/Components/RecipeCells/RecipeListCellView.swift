e7be7579fe73bc7c987bf3a010d47cf24b63d492
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-06-11 17:51:28 -0300

    Added recipe category icon

--- MyFavoriteRecipes/Components/RecipeCells/RecipeListCellView.swift
+++ MyFavoriteRecipes/Components/RecipeCells/RecipeListCellView.swift
@@ -21,16 +21,25 @@ struct RecipeListCellView: View {
             })
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .frame(width: 100, height: 80)
+            .padding(.trailing)
             
-            VStack {
+            VStack(alignment: .leading) {
                 Text(recipe.title)
                     .font(.body)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
                     .truncationMode(.tail)
                 
-                Text(recipe.category)
-                    .font(.caption)
+                Label {
+                    Text(recipe.category)
+                        .font(.subheadline)
+                } icon: {
+                    Image(Categories(rawValue: recipe.category)?.icon ?? "allCategories")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundStyle(.categoriesIcon)
+                        .frame(width: 30)
+                }
             }
         }
     }
